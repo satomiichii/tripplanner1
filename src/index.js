6d5c7dafb23796829b1c99fb1a287c0bf65ea013
Anclaudys <anclaudysdata@gmail.com>
2020-11-23 13:10:23 -0500

    labwithsatomi

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//claer
+//import mapboxgl from "mapbox-gl";
+console.log('works again~')
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYW5jbGF1ZHlzIiwiYSI6ImNraHV1NHh6YzE5NjcyeXFwZzZjdno0MHUifQ.M_fusuuoaF5VHSOwOTJOcw';
+
+// const map = new mapboxgl.Map({
+//     container: "map",
+//     center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+//     zoom: 12, // starting zoom
+//     style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+// });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5jbGF1ZHlzIiwiYSI6ImNraHV1NHh6YzE5NjcyeXFwZzZjdno0MHUifQ.M_fusuuoaF5VHSOwOTJOcw';
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
